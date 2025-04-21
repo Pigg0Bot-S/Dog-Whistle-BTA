07582bb786c70316a3637a26c9e515bc63b66de5
Pigg0Bot-S <cole.leggett@bartcharter.org>
2025-04-21 15:27:09 -0400

    Init, not working

---
+++ src/main/java/pigg0bot/dogwhistle/item/ItemDogWhistle.java
@@ -0,0 +1,15 @@
+package pigg0bot.dogwhistle.item;
+
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+
+
+public class ItemDogWhistle extends Item {
+	public ItemDogWhistle(String name, String namespaceId, int id) {
+		super(name, namespaceId, id);
+	}
+
+
+}
