402c100dff4bd8136ba253a83bd79ef3ae626acb
Pigg0Bot-S <cole.leggett@bartcharter.org>
2025-04-22 01:51:49 -0400

    The (word I shouldn't say) finally works now!

--- src/main/java/pigg0bot/dogwhistle/DogWhistle.java
+++ src/main/java/pigg0bot/dogwhistle/DogWhistle.java
@@ -1,18 +1,33 @@
 package pigg0bot.dogwhistle;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.render.item.model.ItemModelStandard;
+import net.minecraft.client.render.EntityRenderDispatcher;
+import net.minecraft.client.render.TileEntityRenderDispatcher;
+import net.minecraft.client.render.block.color.BlockColorDispatcher;
+import net.minecraft.client.render.block.model.BlockModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModel;
+import net.minecraft.client.render.item.model.ItemModelDispatcher;
 import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pigg0bot.dogwhistle.utility.ModelBuilder;
 import turniplabs.halplibe.helper.ItemBuilder;
+import turniplabs.halplibe.helper.ModelHelper;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+import turniplabs.halplibe.util.ItemInitEntrypoint;
+import turniplabs.halplibe.util.ModelEntrypoint;
 
 
-public class DogWhistle implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+import java.util.function.Supplier;
+
+
+public class DogWhistle implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint, ItemInitEntrypoint, ModelEntrypoint {
     public static final String MOD_ID = "dogwhistle";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Item dogWhistle;
     @Override
     public void onInitialize() {
         LOGGER.info("DogWhistle initialized.");
@@ -30,16 +45,48 @@ public class DogWhistle implements ModInitializer, RecipeEntrypoint, GameStartEn
 
 	@Override
 	public void beforeGameStart() {
-		int startingItemId = 24000;
 
-		Item dogWhistle = new ItemBuilder(MOD_ID)
-			.setIcon("dogwhistle:item/dog_whistle")
-			.build(new Item("dog.whistle", String.format("%s:item/%s", MOD_ID, "dog_whistle"), startingItemId++));
+
+
+
+
 
 	}
 
 	@Override
 	public void afterGameStart() {
+
+	}
+
+	@Override
+	public void afterItemInit() {
+		int startingItemId = 24000;
+
+		dogWhistle = new ItemBuilder(MOD_ID)
+			.build(new Item("dog.whistle", String.format("%s:item/%s", MOD_ID, "dog_whistle"), startingItemId++));
+	}
+
+	@Override
+	public void initItemModels(ItemModelDispatcher dispatcher) {
+		ModelHelper.setItemModel(dogWhistle, ModelBuilder.BuildTool(dogWhistle));
+	}
+
+	@Override
+	public void initBlockModels(BlockModelDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initEntityModels(EntityRenderDispatcher dispatcher) {
+
+	}
+
+	@Override
+	public void initTileEntityModels(TileEntityRenderDispatcher dispatcher) {
+
+	}
+
+	public void initBlockColors(BlockColorDispatcher dispatcher) {
 
 	}
 }
---
+++ src/main/java/pigg0bot/dogwhistle/utility/ModelBuilder.java
@@ -0,0 +1,29 @@
+package pigg0bot.dogwhistle.utility;
+
+import net.minecraft.client.render.item.model.ItemModel;
+import net.minecraft.client.render.item.model.ItemModelStandard;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
+import net.minecraft.core.item.Item;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public class ModelBuilder {
+
+	public static @NotNull Supplier<ItemModel> BuildTool(Item item, String icon_key) {
+		if (item == null || icon_key == null) {
+			throw new IllegalArgumentException("Parameter must not be null!");
+		}
+
+		return () -> {
+			ItemModelStandard tempModel = new ItemModelStandard(item, null).setRotateWhenRendering().setFull3D();
+			tempModel.icon = TextureRegistry.getTexture(icon_key);
+			return tempModel;
+		};
+	}
+
+	public static @NotNull Supplier<ItemModel> BuildTool(Item item) {
+		return BuildTool(item, item.namespaceID.toString());
+	}
+
+}
